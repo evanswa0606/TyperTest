82948fe14dfa084d6f4ce477aa4f61b1e920e9bf
Evan Swanson <evan@Evans-MBP.attlocal.net>
2020-08-22 13:09:09 -0700

    Removed tuples, only color transfered

--- TyperTest.py
+++ TyperTest.py
@@ -1,5 +1,4 @@
 import curses
-
 def ReadyForExit():
 	curses.echo()
 	stdscr.keypad(False)
@@ -7,13 +6,9 @@ def ReadyForExit():
 	curses.endwin()
 
 def redrawString(): 
-	map_colors = {
-		'green': 2,
-		'red': 1
-	}
 	win.erase()
 	for i in range(0, len(characters_typed)):
-		win.addch(characters_typed[i][0], curses.color_pair(map_colors.get(characters_typed[i][1])))
+		win.addch(string_to_type[i], curses.color_pair(characters_typed[i]))
 	win.addch(string_to_type[len(characters_typed)], curses.A_UNDERLINE)
 	for i in range(len(characters_typed)+1, len(string_to_type)):
 		win.addch(string_to_type[i], curses.color_pair(3))
@@ -41,6 +36,10 @@ def main():
 	curses.noecho()
 	stdscr.keypad(True)
 	curses.cbreak()
+	curses.curs_set(0)
+	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
 	window_height=30
 	window_width=130
@@ -50,11 +49,25 @@ def main():
 	window_startx=int((curses.LINES / 2)-(window_height/2))
 	window_starty=int((curses.COLS/2)-(window_width/2))
 	win=curses.newwin(window_height, window_width, window_startx, window_starty)
+
+	win.border(0)
+
+	outer_window_height=32
+	outer_window_width=132
+
+	outer_window_startx=int((curses.LINES/2)-(outer_window_height/2))
+	outer_window_starty=int((curses.COLS/2)-(outer_window_width/2))
+
+	outer_window=curses.newwin(outer_window_height,outer_window_width,outer_window_startx,outer_window_starty)
+	outer_window.border(0)
+	outer_window.addch('h')
+
+	accuracy_win_startx=window_startx+window_width+5
+	accuracy_win_starty=window_starty+window_height+5
+	# The word accuracy is 8 chars long, plus a space, plus 3 digits max
+	
 	stdscr.border(0)
-	string_to_type="""On the 24th of February, 1815, the look-out at Notre-Dame de la Garde signalled the three-master, the Pharaon from Smyrna, Trieste, and Naples. As usual, a pilot put off immediately, and rounding the Château d’If, got on board the vessel between Cape Morgiou and Rion island. Immediately, and according to custom, the ramparts of Fort Saint-Jean were covered with spectators; it is always an event at Marseilles for a ship to come into port, especially when this ship, like the _Pharaon_, has been built, rigged, and laden at the old Phocee docks, and belongs to an owner of the city. The ship drew on and had safely passed the strait, which some volcanic shock has made between the Calasareigne and Jaros islands; had doubled Pomègue, and approached the harbor under topsails, jib, and spanker, but so slowly and sedately that the idlers, with that instinct which is the forerunner of evil, asked one another what misfortune could have happened on board. However, those experienced in navigation saw plainly that if any accident had occurred, it was not to the vessel herself, for she bore down with all the evidence of being skilfully handled, the anchor a-cockbill, the jib-boom guys already eased off, and standing by the side of the pilot, who was steering the _Pharaon_ towards the narrow entrance of the inner port, was a young man, who, with activity and vigilant eye, watched every motion of the ship, and repeated each direction of the pilot.""" 
-	curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
-	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
-	curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+	string_to_type="It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him."
 	redrawString()
 	
 	while True:
@@ -71,14 +84,14 @@ def main():
 			redrawString()
 
 		elif key == string_to_type[currentPos]:
-			characters_typed.append((key, "green"))
+			characters_typed.append(2)
 			if currentPos==len(string_to_type)-1:
 				ReadyForExit()
 				exit(0)
 			redrawString()
 			currentPos+=1
 		else:
-			characters_typed.append((key, "red"))
+			characters_typed.append(1)
 			if currentPos==len(string_to_type)-1:
 				ReadyForExit()
 				exit(0)
